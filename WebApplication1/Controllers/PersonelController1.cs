2c0cc1b7ea72e31c63e30ed9f693524bfd46e761
evren gençoğlu <evrengencoglu2001@hotmail.com>
2023-07-09 10:51:28 +0300

    .

---
+++ WebApplication1/Controllers/PersonelController1.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+
+namespace WebApplication1.Controllers
+{
+    public class PersonelController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            PersonelController1 p1 = new PersonelController1() { Ad = "Selami", Soyad = "Şahin", Departman = "Müzik", Aktif = true };
+            return View();
+        }
+    }
+}
