b5abf32a7983191acc3434905a39512560378fa5
evren gençoğlu <evrengencoglu2001@hotmail.com>
2023-07-09 11:49:04 +0300

    OrnekController hazırlandı

---
+++ WebApplication1/Controllers/OrnekController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace P016_IlkProjem.Controllers
+{
+    public class OrnekController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
